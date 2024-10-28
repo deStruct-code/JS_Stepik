835e789eefc9d0cbb8cb024e1719dfd5b8c91c6c
Vladimir <vkonyukhoff@gmail.com>
2024-10-28 14:10:29 +0300

    'firs'

---
+++ script.js
@@ -0,0 +1,14 @@
+"use strict"
+
+let name = prompt("What is your name?");
+
+for(let x = 10; x > 0; x--){
+    console.log(x);
+}
+console.log(`Hwllo, ${name}!`);
+
+let x = 1;
+let y = '1';
+let z = x === y;
+
+console.log(z);
